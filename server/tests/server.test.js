dee492f240dfe434d2207d8216f56d53f0e4a9f3
Combii <davidstovle@gmail.com>
2017-12-23 17:18:11 +0100

    Extra test file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -29,6 +29,29 @@ describe('POST /todos', () => {
                 expect(todos[0].text).toBe(text);
                 done();
             }).catch((e) => done(e));
-        })
-    })
-})
+        });
+    });
+
+    it('should not create todo with invalid body data', (done) => {
+        var text = '';
+
+        request(app)
+            .post('/todos')
+            .send({ text })
+            .expect(400)
+            .expect((res) => {
+                expect(res.body.message).toBe("Todo validation failed");
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+});
