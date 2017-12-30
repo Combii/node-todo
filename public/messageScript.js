e035bdcfeec342ccfc77a61f4bff3c04194daa80
Combii <davidstovle@gmail.com>
2017-12-30 13:46:17 +0100

    Awesome new update with ajax

--- public/messageScript.js
+++ public/messageScript.js
@@ -1,19 +1,54 @@
 $(document).ready(() => {
 
-    $("#todoText").focus(function() {
+    get_todos();
+
+    setInterval(get_todos, 10000);
+
+
+    function get_todos() {
+
+        //console.log($("#messageBox").length);
+
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            url: '/todos',
+            success: function (object) {
+
+                //console.log(todos);
+
+                $.each(object.todos, function (index, todo) {
+
+                    var todoText = "<p class='msg'>" + todo.text + "</p>";
+
+                    //Check if already exist
+                    if($("#messageBox").text().match(todo.text)){
+                        console.log(true);
+                    }
+                    else{
+                        console.log(false);
+                        $("#messageBox").append(todoText);
+                    }
+                });
+            }
+        });
+
+    }
+
+    $("#todoText").focus(function () {
         $(this).css("background", "#f3d5bd");
         console.log($(this));
 
     });
 
-    $("#todoText").blur(function() {
-       $(this).css("background", "#fff");
+    $("#todoText").blur(function () {
+        $(this).css("background", "#fff");
     });
 
-    $("#sendTodo").click(() =>{
-       var todoText = $("#todoText").val();
+    $("#sendTodo").click(() => {
+        var todoText = $("#todoText").val();
 
-       var frontendText = "<p class='msg'>" + todoText + "</p>";
+        var frontendText = "<p class='msg'>" + todoText + "</p>";
 
         console.log(todoText);
 
@@ -25,21 +60,21 @@ $(document).ready(() => {
             data: JSON.stringify(data),
             contentType: 'application/json',
             url: '/todos',
-            success: function(data) {
+            success: function (data) {
                 console.log('success');
                 console.log(JSON.stringify(data));
             }
         });
 
 
-       $("#messageBox").append(frontendText)
+        $("#messageBox").append(frontendText)
     });
 
     const colors = ["yellow", "pink", "red", "green"];
     let index = 0;
 
 
-    $("#changeBorderColor").click(() =>{
+    $("#changeBorderColor").click(() => {
         /*colors.forEach(color => {
             $("#messageBox").css("border", color)
         });*/
@@ -51,7 +86,7 @@ $(document).ready(() => {
 
 
     function changeColors() {
-        if(index >= colors.length)
+        if (index >= colors.length)
             index = 0;
 
         return colors[index];
