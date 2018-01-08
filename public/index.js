9758290bacfa349d73f6259e2b998a7be3cace29
Combii <davidstovle@gmail.com>
2018-01-08 14:48:52 +0100

    Working GET. And added old js

--- public/index.js
+++ public/index.js
@@ -1,10 +1,64 @@
 $(document).ready(function () {
+
+    var localTodoList = [];
+
+    get_todos();
+
+    function get_todos() {
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            url: '/todos',
+            success: function (object) {
+                setup_todos(object.todos);
+            }
+        });
+    }
+
+
+    function setup_todos(todoList) {
+
+        $.each(todoList, function (index, todo) {
+            if (searchList(todo._id) === null) {
+                localTodoList.push({
+                    id: todo._id,
+                    text: todo.text,
+                    completed: todo.completed,
+                    completedAt: todo.completedAt
+                });
+                setupTodoFrontend(localTodoList[localTodoList.length - 1]);
+            }
+        });
+    }
+
+    function setupTodoFrontend(todo) {
+
+        $("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + todo.text + "<a class='remove'>x</a><hr></li>");
+
+    }
+
+
+    function searchList(object) {
+
+        var todo = null;
+
+        $.each(localTodoList, function (index, todoLocal) {
+            if (todoLocal.id === object) {
+                todo = todoLocal;
+                return false;
+            }
+        });
+        return todo;
+    }
+
+
+    // POST
     $(".add-items").submit(function (event) {
         event.preventDefault();
         //console.log('test');
         var item = $("#todo-list-item").val();
 
-        if (item) {
+        if (item.trim() !== "") {
             $("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>")
             $("#todo-list-item").val("");
         }
@@ -14,7 +68,7 @@ $(document).ready(function () {
         console.log('Checkbox checked');
     })*/
 
-
+    // PATCH
     $(document).on("change", ".checkbox", function () {
 
         if ($(this).is(':checked')) {
@@ -31,7 +85,7 @@ $(document).ready(function () {
         //console.log($(this).parent().text());
     });
 
-
+    // DELETE
     $(document).on("click", ".remove", function () {
 
         $(this).parent().remove();
